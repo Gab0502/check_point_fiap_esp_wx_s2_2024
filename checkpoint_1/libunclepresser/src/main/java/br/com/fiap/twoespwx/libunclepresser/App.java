26245d1fa6dec526393b56411fdfdacb34b57c7c
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-04 20:52:49 -0300

    init

--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
@@ -3,12 +3,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Grupo: Batata Doce
+ * Grupo: ScriptFlow
  * Autores: 
- *      - João da Silva     -   jaodasilva@example.com
- *      - Jose da Silva     -   zedasilva@example.com
- *      - Maria da Silva    -   marydasilva@example.com
- *      - Monica da Silva   -   nicadasilva@example.com
+ *      - Enricco Rosse de Souza Carvalho     -   rm551717@fiap.com.br
+ *      - Gabriel Marquez Trevisan     -   rm99227@fiap.com.br
+ *      - Guilherme Silva dos Santos    -   rm551168@fiap.com.br
+ *      - Samuel Ramos de Almeida   -   rm991134@fiap.com.br
  */
 
 package br.com.fiap.twoespwx.libunclepresser;
@@ -18,6 +18,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("LIB UNCLE PRESSER - GRUPO BATATA-DOCE");
+        String inputFilename = "checkpoint_1/libunclepresser/inputs/input1.txt";
+        String outputFilename = "checkpoint_1/libunclepresser/outputs/OUTPUT1.TXT";
+
+        NucleotidioInputReader nir = new NucleotidioInputReader();
+        NucletidioOutputWriter now = new NucletidioOutputWriter();
+
+        List<String> lines = nir.readWithScanner(inputFilename);
+
+        for (String line : lines){
+            System.out.println(line);
+        }
+
+
     }
 }
+
+
